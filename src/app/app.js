41a6efb65774fe4ca36585694907e9a3995c2a16
jblh <48096357+jblh@users.noreply.github.com>
2022-03-28 17:14:55 +0200

    test

--- src/app/app.js
+++ src/app/app.js
@@ -32,6 +32,16 @@ app.use('/', cardsRoute)
 app.use('/', guard, editionsRoute)
 // app.use('/', guard, cardsRoute)
 
+// testing
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.use('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+// testing
+
 app.use((_req, res) => {
   res.status(HttpCodes.NOT_FOUND).json({ message: 'Not found' })
 })
--- src/app/server.js
+++ src/app/server.js
@@ -6,16 +6,6 @@ const createFolderIsNotExist = require('../helpers/createFolder')
 const PORT = process.env.PORT || 3000
 const UPLOAD_DIR = process.env.UPLOAD_DIR
 
-// testing
-
-app.use(express.static(path.join(__dirname, 'build')))
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-
-// testing
-
 db.then(() => {
   app.listen(PORT, async () => {
     await createFolderIsNotExist(UPLOAD_DIR)

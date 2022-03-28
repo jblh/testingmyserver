c559e5496d9dfcee057723cc0a0ced5277b641fa
jblh <48096357+jblh@users.noreply.github.com>
2022-03-28 17:00:44 +0200

    test

--- src/app/app.js
+++ src/app/app.js
@@ -34,11 +34,11 @@ app.use('/', guard, editionsRoute)
 
 // testing
 
-app.use(express.static(path.join(__dirname, 'build')))
+// app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'))
+// })
 
 // testing
 
